b276de645221a93bc025cce36b6ab11900040186
Iqsyal Maulana <86776072+iqsyal21@users.noreply.github.com>
2024-02-15 11:01:35 +0700

    Update routes.tsx

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -13,6 +13,7 @@ import Search from "../pages/dashboard/quotes/Search";
 
 const routes = createRoutesFromElements(
   <>
+    <Route path="*" element={<NotFound />} />
     <Route path="/dashboard" element={<Layout />}>
       <Route index element={<Dashboard />} />
       <Route path="quotes" element={<Quotes />} />
@@ -23,7 +24,6 @@ const routes = createRoutesFromElements(
     </Route>
     <Route path="/login" element={<Login />} />
     <Route path="/" element={<Home />} />
-    <Route path="*" element={<NotFound />} />
   </>
 );
 
